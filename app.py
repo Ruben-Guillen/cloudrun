5c40fd401ac65d14dfe6a1c128e7835abd75a031
Ruben <rguillen1381@sdsu.edu>
2025-05-07 13:36:38 -0700

    changing the styling

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, send_from_directory
 import os
 
-app = Flask(__name__, static_folder='templates')
+app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def index():
@@ -14,6 +14,10 @@ def page2():
 @app.route('/page3')
 def page3():
     return send_from_directory('templates', 'page3.html')
+
+@app.route('/welcome')
+def welcome():
+    return send_from_directory('templates', 'welcome.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080) 
